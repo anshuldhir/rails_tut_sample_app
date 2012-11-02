a9907278cc6c504d215f8c648cc8fd9854c3ebcf
Anshul Dhir <anshul@dhirs.org>
2012-11-02 16:39:40 -0500

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 RailsTutSampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  get "users/new"  # keeping it here for now, to keep the users/new route working.
+
+  root to: 'static_pages#home'
+  match '/help', to: 'static_pages#help'
+  match '/about', to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+
+  match '/signup', to: 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
